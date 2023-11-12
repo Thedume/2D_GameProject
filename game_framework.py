054c1c75ea7f82128ba8ac57306909592b51ef72
JoJinyoung <jjojin7168@gmail.com>
2023-11-12 16:52:56 +0900

    버그 수정

--- game_framework.py
+++ game_framework.py
@@ -52,6 +52,8 @@ def run(start_mode):
         stack[-1].update()
         stack[-1].draw()
         frame_time = time.time() - current_time
+        if frame_time == 0.0:
+            continue
         frame_rate = 1.0 / frame_time
         current_time += frame_time
         # print(f'Frame Time: {frame_time}, Frame Rate: {frame_rate}')

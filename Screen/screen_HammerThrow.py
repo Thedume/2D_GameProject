e7a4f2bcdfeeb7f0276f10dbb4a028aa6585f17e
JoJinyoung <jjojin7168@gmail.com>
2023-11-27 04:05:21 +0900

    헤머 플레이어 생성

--- Screen/screen_HammerThrow.py
+++ Screen/screen_HammerThrow.py
@@ -1,29 +1,40 @@
 from pico2d import *
 import game_framework
 import Screen.menu_screen
+import game_world
+from Class.player_Hammer import HammerPlayer
 
 
 def init():
     global image
     global text
+    global player
 
     running = True
     # image = load_image('./resources/title.png')
     text = load_font("./resources/ENCR10B.TTF", 16)
 
+    player = HammerPlayer()
+    game_world.add_object(player, 1)
+
 
 def finish():
+    game_world.clear()
     pass
 
 
 def update():
+    game_world.update()
+
+    game_world.handle_collisions()
     pass
 
 
 def draw():
     clear_canvas()
     # image.draw(400, 300)
-    text.draw(400, 300, "Hammer Screen", (255, 255, 0))
+    # text.draw(400, 300, "Hammer Screen", (255, 255, 0))
+    game_world.render()
     update_canvas()
     pass
 
@@ -34,6 +45,8 @@ def handle_events():
             game_framework.quit()
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
             game_framework.pop_mode()
+        else:
+            player.handle_event(event)
 
 
 def pause():

f8a6a626caf36f091c765071668435b1ea09f344
JoJinyoung <jjojin7168@gmail.com>
2023-11-29 18:48:52 +0900

    코드 정리

--- Class/angle.py
+++ Class/angle.py
@@ -6,17 +6,15 @@ class Angle:
         self.angle_img = load_image('./resources/hammer/angle.png') # 이미지 수정해야함
         self.arrow = load_image('./resources/hammer/arrow.png')
         self.x, self.y = x, y
-        self.deg = 15 # -15 ~ 15
+        self.deg = 0 # -15 ~ 15 : Foul
         self.isRotate = True
 
     def update(self):
-        if not (-15 <= self.deg <= 15):
-            pass # 파울
         if self.isRotate:
-            self.deg += 0.5
-
-        pass
+            self.deg += 1.0
+            if self.deg == 360:
+                self.deg = 0
 
     def draw(self):
         self.angle_img.draw(self.x, self.y, 50, 50)
-        self.arrow.composite_draw(3.141592 / 180 * self.deg, '', self.x, self.y) #
+        self.arrow.composite_draw(3.141592 / 180 * self.deg, '', self.x, self.y)

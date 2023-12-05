9420ed67621620463937d22b4ce1c6f49425e254
JoJinyoung <jjojin7168@gmail.com>
2023-12-06 03:55:08 +0900

    창던지기에서 사용할 앵글 사진 추가 및 수치 추가

--- Class/angle.py
+++ Class/angle.py
@@ -1,15 +1,21 @@
 from pico2d import *
 
+angle_state = {'hammer': 50, 'javelin': 30}
+
 
 class Angle:
     def __init__(self, x, y, use):
-        self.angle_img = load_image('./resources/hammer/angle.png') # 이미지 수정해야함
+        if use == 'hammer':
+            self.angle_img = load_image('./resources/hammer/angle1.png')
+        elif use == 'javelin':
+            self.angle_img = load_image('./resources/hammer/angle2.png')
         self.arrow = load_image('./resources/hammer/arrow.png')
         self.x, self.y = x, y
         self.deg = 0 # -15 ~ 15 : Foul
         self.deg_dir = 1
         self.isRotate = True
         self.use = use
+        self.size = angle_state[use]
 
     def update(self):
         match self.use:
@@ -24,8 +30,6 @@ class Angle:
                     if (self.deg == 90.0 and self.deg_dir == 1) or (self.deg == 0.0 and self.deg_dir == -1):
                         self.deg_dir *= -1
 
-
-
     def draw(self):
-        self.angle_img.draw(self.x, self.y, 50, 50)
+        self.angle_img.draw(self.x, self.y, self.size, 50)
         self.arrow.composite_draw(3.141592 / 180 * self.deg, '', self.x, self.y)

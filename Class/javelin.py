98801a882adca34f9e27fd0cb1356b6433d26a94
JoJinyoung <jjojin7168@gmail.com>
2023-12-06 03:55:41 +0900

    창 던져질때의 움직임 추가

--- Class/javelin.py
+++ Class/javelin.py
@@ -1,14 +1,16 @@
 from pico2d import *
 
+import game_world
 
-def calculate_position(angle, force, time):
+
+def calculate_position(angle, force, time, x, y):
     # 초기 속도 계산
     velocity_x = force * math.cos(math.radians(angle))
     velocity_y = force * math.sin(math.radians(angle))
 
     # 위치 계산
-    x = velocity_x * time
-    y = (velocity_y * time) - (0.5 * 9.8 * time**2)  # 중력 가속도 고려
+    x = (velocity_x * time) * 0.005 + x
+    y = ((velocity_y * time) - (0.5 * 9.8 * time**2)) * 0.005 + y  # 중력 가속도 고려
 
     return x, y
 
@@ -16,17 +18,26 @@ def calculate_position(angle, force, time):
 class Javelin:
     def __init__(self, x, y, a, p):
         self.javelin_img = load_image('./resources/javelin/javelin.png')
+        self.oy = y
         self.x, self.y = x, y
         self.angle, self.power = a, p
         self.isMove = False
         self.time = 0
         self.dt = 0.1
+        self.state = 'move'
+
+        # print(a, p)
 
     def update(self):
-        self.x, self.y = calculate_position(self.angle, self.power, self.time)
-
+        if self.y >= self.oy and self.state == 'move':
+            self.x, self.y = calculate_position(self.angle, self.power, self.time, self.x, self.y)
+        else:
+            self.state = 'stop'
+        # print(self.x, self.y)
+        self.angle -= self.dt
         self.time += self.dt
+        delay(0.025)
         pass
 
     def draw(self):
-        self.javelin_img.draw(self.x, self.y)
+        self.javelin_img.composite_draw(3.141592 / 180 * self.angle, '',self.x, self.y)
